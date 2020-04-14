7ca2c9c982a21089bdf6b5b105fd5178433d561c
Gheorghe Tarcea <tarcea@gmail.com>
2020-04-14 11:02:25 +0200

    valid_card method implemented git push origin master

--- lib/credit_card_checker.rb
+++ lib/credit_card_checker.rb
@@ -14,4 +14,41 @@ end
 
 def valid_card?(card)
   # TODO: Implement the validator. Return true if the card is valid, false otherwise.
+  if card.length < 16
+    false
+  else
+    # eliminate spaces from "card"
+    no_spaces = card.delete(' ')
+    # luhn algo:
+    # double the value of every second digit
+    my_array = no_spaces.split('')
+    # p my_array
+    my_map = my_array.map.with_index do |el, i|
+      el = el.to_i
+      i.even? ? el + el : el
+    end
+    # p my_map
+    # if the result of doubling is > 9, then add the two digits
+    step2_map = my_map.map do |element|
+      if element > 9
+        mini_array = element.to_s.split('')
+        mini_array[0].to_i + mini_array[1].to_i
+      else
+        element
+      end
+    end
+    # p step2_map
+    sum = 0
+    step2_map.each { |element| sum += element }
+    if sum.zero?
+      true
+    elsif sum > 9
+      mini_array = sum.to_s.split('')
+      if mini_array[1].to_i.zero?
+        true
+      else
+        false
+      end
+    end
+  end
 end
